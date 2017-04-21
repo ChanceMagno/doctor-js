9498b273e034d87eb7f30c2534294c3dc84d5e58
Chance <ChanceMagno@gmail.com>
2017-04-21 15:56:50 -0700

    add getDoctors method, verified getting objects returned

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,41 +1,55 @@
-
 var Doctor = require('./../js/doctor.js').doctorModule;
 
 $(document).ready(function() {
   currentDoctor = new Doctor();
-
+//Reactive form
   $('#nameSearch').click(function() {
     $('#specialtySearch, #practiceSearch, #locationSearch, #searchSubmit').show("slow");
     $('.specialtyForm, .practiceForm, .locationForm, .sortOption, #nameSearch').hide("fast");
     $('.sortOption, .nameForm').slideDown("slow");
-  })
+  });
 
   $('#specialtySearch').click(function() {
     $('#nameSearch, #practiceSearch, #locationSearch, #searchSubmit').show("slow");
     $('.nameForm, .locationForm, .practiceForm, .sortOption, #specialtySearch').hide("fast");
     $('.sortOption, .specialtyForm').slideDown("slow");
-  })
+  });
 
   $('#practiceSearch').click(function() {
     $('#specialtySearch, #nameSearch, #locationSearch, #searchSubmit').show("slow");
     $('.specialtyForm, .locationForm, .nameForm, .sortOption, #practiceSearch').hide("fast");
     $('.sortOption, .practiceForm').slideDown("slow");
-  })
+  });
 
   $('#locationSearch').click(function() {
     $('#specialtySearch, #practiceSearch, #nameSearch, #searchSubmit').show("slow");
     $('.nameForm, .specialtyForm, .practiceForm, .sortOption, #locationSearch').hide("fast");
     $('.sortOption, .locationForm').slideDown("slow");
-  })
+  });
+//End Reactive form
 
+//Search Submit (determines which search method to call and if for is empty)
   $('#searchSubmit').click(function() {
     var name = $("form #nameInput").val();
     var specialty = $("form #specialtyInput").val();
     var practice = $("form #practiceInput").val();
     var location = $("form #locationInput").val();
-    if (null == name || name == "" && null == specialty || specialty == "" && null == practice || practice == "" && null == location || location == "") {
-      alert("Search Field is Empty");
-    } else {alert("not blank?");}
-  })
-
-})
+    var sortBy = $("form #sortOption").val();
+  //   if ($('form #nameInput').is(":visible")) {
+  //     currentDoctor.searchName(name);
+  //   } else if ($('form #specialtyInput').is(":visible")) {
+  //       currentDoctor.searchLocation(location);
+  //   } else if ($("form #specialtyInput").is(":visible")) {
+  //       currentDoctor.searchSpecialty(specialty);
+  //   } else if ()
+  // }
+    // var name = $("form #nameInput").val();
+    // var specialty = $("form #specialtyInput").val();
+    // var practice = $("form #practiceInput").val();
+    // var location = $("form #locationInput").val();
+    // var sortBy = $("form #sortOption").val();
+    //
+    currentDoctor.getDoctors(name, specialty, practice, location, sortBy);
+});
+//end Search Submit
+});
--- js/doctor.js
+++ js/doctor.js
@@ -3,16 +3,42 @@ var apiKey = require('./../.env').apiKey
 function Doctor() {
 }
 
+var apiName;
+var apiSpecialty;
+var apiPractice;
+var apiLocation;
+var apiSortBy;
 
-
-Doctor.prototype.getDoctors = function() {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
-      console.log(result);
-    })
-   .fail(function(error){
-      console.log("fail");
-    });
+//manipulates api call based on users search inputs
+Doctor.prototype.getDoctors = function(name, specialty, practice, location, sortBy) {
+  if (name == "") {
+    apiName = name;
+  } else { apiName = ("name=" + name);
+    }
+  if (specialty == "") {
+    apiSpecialty = specialty;
+  } else { apiSpecialty = ("&specialty_uid=" + specialty);
+    }
+  if (practice == "") {
+    apiPractice = practice;
+  } else { apiPractice = ("&practice_uid=" + practice);
+    }
+  if (location == "") {
+    apiLocation = location;
+  } else { apiLocation = ("&location=" + location);
+    }
+  if (sortBy == "") {
+    apiSortBy = sortBy;
+  } else { apiSortBy = ("&sort=" + sortBy);
+    }
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?'+ apiName + apiSpecialty + apiPractice + apiLocation + '&user_location=37.773%2C-122.413' + apiSortBy + '&skip=0&limit=10&user_key=' + apiKey).then(function(result) {
+            console.log(result);
+      })
+     .fail(function(error){
+        console.log("fail");
+      });
 };
 
 exports.doctorModule = Doctor;
+
+'https://api.betterdoctor.com/2016-03-01/doctors?name=Jim&specialty_uid=sadf&practice_uid=asdf&location=portland&user_location=37.773%2C-122.413&sort=full-name-asc&skip=0&limit=10&user_key=fe19389833306d02643db671a806c649'
