c60bfb439d4f6940bebdf7b68d75fd1cba3a6e56
Chance <ChanceMagno@gmail.com>
2017-04-23 19:57:38 -0700

    add feature to load more search results when scrolling down

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -5,44 +5,68 @@ var specialty;
 var practice;
 var location;
 var sortBy;
+var searchCriteria = [];
 
 $(document).ready(function() {
-  currentDoctor = new Doctor();
+currentDoctor = new Doctor();
 //Reactive form
   $('#nameSearch').click(function() {
     $('#specialtySearch, #practiceSearch, #locationSearch, #searchSubmit').show("slow");
-    $('.specialtyForm, .practiceForm, .locationForm, .sortOption, #nameSearch').hide("fast");
+    $('.specialtyForm, .practiceForm, .locationForm, #nameSearch, .specialtyWarning').hide("fast");
     $('.sortOption, .nameForm').slideDown("slow");
+    $("#nameSearch").bind("click", function() {
+      $("form #specialtyInput[type=text], textarea").val("");
+      $("form #locationInput[type=text], textarea").val("");
+      $("form #practiceInput[type=text], textarea").val("");
+    });
   });
 
   $('#specialtySearch').click(function() {
-    $('#nameSearch, #practiceSearch, #locationSearch, #searchSubmit').show("slow");
-    $('.nameForm, .locationForm, .practiceForm, .sortOption, #specialtySearch').hide("fast");
-    $('.sortOption, .specialtyForm').slideDown("slow");
+    $('#nameSearch, #practiceSearch, #locationSearch, #searchSubmit, .specialtyWarning').show("slow");
+    $('.locationForm, .practiceForm, nameForm, #specialtySearch').hide("fast");
+    $('.nameForm .sortOption, .specialtyForm').slideDown("slow");
+    $("#specialtySearch").bind("click", function() {
+      $("form #locationInput[type=text], textarea").val("");
+      $("form #practiceInput[type=text], textarea").val("");
+    });
   });
 
   $('#practiceSearch').click(function() {
     $('#specialtySearch, #nameSearch, #locationSearch, #searchSubmit').show("slow");
-    $('.specialtyForm, .locationForm, .nameForm, .sortOption, #practiceSearch').hide("fast");
+    $('.specialtyForm, .locationForm, .nameForm, #practiceSearch, .specialtyWarning').hide("fast");
     $('.sortOption, .practiceForm').slideDown("slow");
+    $("#praciteSearch").bind("click", function() {
+      $("form #specialtyInput[type=text], textarea").val("");
+      $("form #locationInput[type=text], textarea").val("");
+      $("form #nameInput[type=text], textarea").val("");
+    });
   });
 
   $('#locationSearch').click(function() {
     $('#specialtySearch, #practiceSearch, #nameSearch, #searchSubmit').show("slow");
-    $('.nameForm, .specialtyForm, .practiceForm, .sortOption, #locationSearch').hide("fast");
+    $('.nameForm, .specialtyForm, .practiceForm, #locationSearch, .specialtyWarning').hide("fast");
     $('.sortOption, .locationForm').slideDown("slow");
+    $("#locationSearch").bind("click", function() {
+      $("form #specialtyInput[type=text], textarea").val("");
+      $("form #nameInput[type=text], textarea").val("");
+      $("form #practiceInput[type=text], textarea").val("");
+    });
   });
 //End Reactive form
 
 //Search Submit
   $('#searchSubmit').click(function() {
+    skipNumber = 0;
+    searchCriteria = [];
     var name = $("form #nameInput").val();
     var specialty = $("form #specialtyInput").val();
     var practice = $("form #practiceInput").val();
     var location = $("form #locationInput").val();
     var sortBy = $("form #sortOption").val();
     $(".searchResults").empty();
+    searchCriteria.push(name, specialty, practice, location, sortBy);
     currentDoctor.getDoctors(name, specialty, practice, location, sortBy, displayDoctors);
+
 
 });
 //end Search Submit
@@ -58,6 +82,16 @@ var displayDoctors = function(searchResults) {
     });
   }
 };
+
+
+
+// for loading page with more search results
+$(window).scroll(function() {
+    if($(window).scrollTop() == $(document).height() - $(window).height()) {
+           currentDoctor.next();
+           currentDoctor.getDoctors(searchCriteria[0], searchCriteria[1], searchCriteria[2], searchCriteria[3], searchCriteria[4], displayDoctors);
+    }
+  });
 
 
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,4 +1,5 @@
 var apiKey = require('./../.env').apiKey;
+
 var apiName;
 var apiSpecialty;
 var apiPractice;
@@ -8,48 +9,55 @@ var skipNumber = 0;
 
 
 function Doctor() {
+
 }
+
+Doctor.prototype.next = function() {
+  skipNumber = skipNumber + 10;
+};
 
 //manipulates api call based on users search inputs/data results are passed to front end function
 Doctor.prototype.getDoctors = function(name, specialty, practice, location, sortBy, displayDoctors) {
-  if (name === "") {
-    apiName = name;
-  } else { apiName = ("name=" + name);
-    }
-  if (specialty === "") {
-    apiSpecialty = specialty;
-  } else { apiSpecialty = ("specialty_uid=" + specialty);
-    }
-  if (practice === "") {
-    apiPractice = practice;
-  } else { apiPractice = ("practice_uid=" + practice);
-    }
-  if (location === "") {
-    apiLocation = location;
-  } else { apiLocation = ("location=" + location);
-    }
-  if (sortBy === "") {
-    apiSortBy = sortBy;
-  } else { apiSortBy = ("&sort=" + sortBy);
-    }
-    $.get('https://api.betterdoctor.com/2016-03-01/doctors?'+ apiName + apiSpecialty + apiPractice + apiLocation + '&user_location=37.773%2C-122.413' + apiSortBy + '&skip='+ skipNumber + '&limit=10&user_key=' + apiKey).then(function(response) {
 
-      // https://api.betterdoctor.com/2016-03-01/doctors?location=portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=fe19389833306d02643db671a806c649
+    if (name === "") {
+      apiName = name;
+    } else { apiName = ("name=" + name);
+      }
+    if (specialty === "") {
+      apiSpecialty = specialty;
+    } else { apiSpecialty = ("&specialty_uid=" + specialty);
+      }
+    if (practice === "") {
+      apiPractice = practice;
+    } else { apiPractice = ("practice_uid=" + practice);
+      }
+    if (location === "") {
+      apiLocation = location;
+    } else { apiLocation = ("&location=" + location);
+      }
+    if (sortBy === "") {
+      apiSortBy = sortBy;
+    } else { apiSortBy = ("&sort=" + sortBy);
+      }
 
-        var searchResults = [];
-        response.data.forEach(function(result){
-          var search = {};
-          search.firstName = result.profile.first_name;
-          search.lastName = result.profile.last_name;
-          search.image = result.profile.image_url;
-          search.bio = result.profile.bio;
-          searchResults.push(search);
+      $.get('https://api.betterdoctor.com/2016-03-01/doctors?'+ apiName + apiSpecialty + apiPractice + apiLocation + '&user_location=37.773%2C-122.413' + apiSortBy + '&skip='+ skipNumber + '&limit=10&user_key=' + apiKey).then(function(response) {
+
+          var searchResults = [];
+          response.data.forEach(function(result){
+            var search = {};
+            search.firstName = result.profile.first_name;
+            search.lastName = result.profile.last_name;
+            search.image = result.profile.image_url;
+            search.bio = result.profile.bio;
+            searchResults.push(search);
+          });
+            displayDoctors(searchResults);
+        })
+       .fail(function(error){
+          $('.searchResults').text(error.responseJSON.message);
         });
-          displayDoctors(searchResults);
-      })
-     .fail(function(error){
-        $('.searchResults').text(error.responseJSON.message);
-      });
-};
+  };
+
+
 
 exports.doctorModule = Doctor;
